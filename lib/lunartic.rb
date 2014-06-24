e15e5c96118b8d8f82af1e931e791b02450b39d7
Brian O'Keefe <brian@bokstuff.com>
2014-06-24 17:21:52 -0400

    Implement convenience methods

--- lib/lunartic.rb
+++ lib/lunartic.rb
@@ -1,14 +1,15 @@
+require 'date'
 require 'lunartic/version'
 require 'lunartic/moon'
 
 module Lunartic
   class << self
     def now
-
+      Lunartic::Moon.new Date.today
     end
 
     def on_date(date)
-
+      Lunartic::Moon.new date
     end
   end
 end
